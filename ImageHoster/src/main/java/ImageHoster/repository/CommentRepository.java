008f5731d37d39d1d27fac6a0a2c5686e8ec347a
pankaj-kalla <pankajkalla@outlook.com>
2019-07-15 22:26:32 +0530

    Commit-2

---
+++ ImageHoster/src/main/java/ImageHoster/repository/CommentRepository.java
@@ -0,0 +1,37 @@
+package ImageHoster.repository;
+
+import ImageHoster.model.Comment;
+import ImageHoster.model.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
+
+@Repository
+public class CommentRepository {
+    //Get an instance of EntityManagerFactory from persistence unit with name as 'imageHoster'
+    @PersistenceUnit(unitName = "imageHoster")
+    private EntityManagerFactory emf;
+
+    //The method receives the User object to be persisted in the database
+    //Creates an instance of EntityManager
+    //Starts a transaction
+    //The transaction is committed if it is successful
+    //The transaction is rolled back in case of unsuccessful transaction
+
+    public void CreateComments(Comment newcomment) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+            //persist() method changes the state of the model object from transient state to persistence state
+            em.persist(newcomment);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+    }
+}
